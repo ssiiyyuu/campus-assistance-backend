0e4c2b126fcd8d57ab4b75ea8ce90ab9a7ed42ba
siyu <504826715@qq.com>
2024-06-05 10:31:30 +0800

    add notification

---
+++ campus-assistance-server/src/main/java/com/siyu/server/service/UserNotificationReadService.java
@@ -0,0 +1,16 @@
+package com.siyu.server.service;
+
+import com.siyu.server.entity.UserNotificationRead;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author MybatisPlusGenerator
+ * @since 2024-06-01 10:01:01
+ */
+public interface UserNotificationReadService extends IService<UserNotificationRead> {
+
+}
